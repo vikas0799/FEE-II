fa955c798a62eeb32a9849a9526dc1b2485dd31c
Vikas Patel <vikaspatel07071999@gmail.com>
2024-02-15 20:59:44 +0530

    js basics

---
+++ G30/lect-27-js basics/app.js
@@ -0,0 +1,35 @@
+// console.log("hello");
+// let x=prompt("enter a number");
+// console.log(x);
+// console.log(typeof(x));
+
+// // x=parseInt(x);
+// // console.log(typeof(x));
+// // console.log(x);
+
+
+// x=parseFloat(x);
+// console.log(typeof(x));
+// console.log(x*x);
+
+
+// alert("form submitted");
+// let name1=prompt("eneter your name");
+// console.log(name1);
+
+
+// let x=eval(3+4+5-1-3);
+// console.log((x));
+
+// let arr=[23,34,65,"vikas","lakshay",true,null,null,false];
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+sum(2,6);
+function sum(a,b){
+    let ans=a+b;
+    console.log(ans);
+}
+// sum(2,5);
+
