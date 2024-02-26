38850c0c6eb058afcf2ef36e591f17333cda07e7
Vikas Patel <vikaspatel07071999@gmail.com>
2024-02-27 00:39:52 +0530

    js dom

---
+++ G10/lect-32-DOM/app.js
@@ -0,0 +1,15 @@
+// console.log("hello");
+// let x=document.body;
+// console.log(x);
+
+// console.log(window);
+// prompt("enter a number");
+// let x=window.prompt("enter a number");
+// console.log(x);
+
+// let ans=window;
+// console.log(ans);
+
+document.body.style.background="white";
+document.body[1].style.background="red";
+
---
+++ G30/lect-32-js-dom/app.js
@@ -0,0 +1,42 @@
+// // console.log("hello g7");
+
+// // // let ans=document;
+// // let ans=document.head;
+// // console.log(ans);
+
+// // document.body.style.background="green";
+
+
+
+// // let x=prompt("enter a number");
+// // let x=window.prompt("enter a number");
+// // console.log(x);
+
+
+// // let ans=window;
+// // console.log(ans);
+
+// // window.document.body.style.color="red";
+
+// const url = 'https://shopifyvolodimir-kudriachenkov1.p.rapidapi.com/activateApplicationCharge';
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/x-www-form-urlencoded',
+// 		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+// 		'X-RapidAPI-Host': 'Shopifyvolodimir-kudriachenkoV1.p.rapidapi.com'
+// 	},
+// 	body: new URLSearchParams({
+// 		accessToken: '<REQUIRED>',
+// 		shopName: '<REQUIRED>',
+// 		applicationChargeId: '<REQUIRED>'
+// 	})
+// // };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// // }
