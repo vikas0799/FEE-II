f337150a95389ce59a5321de1ecbc8c91e71c58e
Vikas Patel <vikaspatel07071999@gmail.com>
2024-02-27 11:57:11 +0530

    js dom

---
+++ G10/lect-33-js dom/app.js
@@ -0,0 +1,44 @@
+// console.log("hello g10");
+// let ans=document.getElementById("box2");
+// console.log(ans);
+// ans.style.backgroundColor="red";
+
+// let ans= document.getElementsByClassName("box");
+// console.log(ans);
+// ans[0].style.background="red";
+
+
+// let ans=document.getElementsByTagName("p");
+// console.log(ans);
+// ans[0].style.color="green";
+// ans[1].innerHTML="hello world";
+
+//    let ans= document.querySelector("p");
+// let ans=document.querySelectorAll("p");
+//    console.log(ans);
+//    ans[1].style.fontWeight="bold";
+
+// let ans=document.getElementById("box2");
+// ans.classList.add("circle");
+
+// let box=document.getElementsByClassName("box");
+// box[0].classList.add("circle");
+// console.log(box[1]);
+// box[0].classList.remove("circle");
+// box[0].classList.toggle("circle");
+
+
+// DOM TraversAL
+//  let container=document.getElementById("box2");
+let container=document.getElementsByClassName("box");
+//  console.log(container.childElementCount);
+ console.log(container[0].children[0]);
+// console.log(container[0].parentElement.children);
+// console.log(container[0].childNodes);
+// console.log(contain.nextElementSibling);
+// console.log(contain.parentElement.children[2].firstElementChild);
+// console.log(contain.previousElementSibling);
+
+
+
+
---
+++ G7/lect-33-js dom/app.js
@@ -0,0 +1,44 @@
+// console.log("hello");
+//   let ans=  document.getElementById("box2");
+//   console.log(ans);
+//  ans.style.backgroundColor="red";
+// ans.style.color="white";
+
+
+//  let ans=document.getElementsByClassName("container");
+//  console.log(ans);
+//  ans[0].style.backgroundColor="red";
+//  ans[1].innerHTML="hello g7";
+
+// let ans=document.getElementsByTagName("p");
+// console.log(ans);
+// ans[1].innerHTML="hello g7 is bad batvhc";
+// ans[0].style.color="red";
+
+
+//  let ans= document.querySelector("p");
+// let ans=document.querySelectorAll("p");
+//  console.log(ans);
+//  ans[0].style.color="red";
+
+
+
+let box2=document.getElementsByClassName("box");
+// box2[1].classList.add("circle");
+// box2[1].classList.add("circle");
+// console.log(box2[1]);
+// box2[0].classList.remove("circle");
+// box2[0].classList.toggle("circle");
+
+// DOM TraversAL
+ let container=document.getElementsByClassName("container");
+//  console.log(container[0].childElementCount);
+//  console.log(container[0].children[1]);
+console.log(container[0].parentElement.children);
+// console.log(container[0].childNodes);
+// console.log(contain.nextElementSibling);
+// console.log(contain.parentElement.children[2].firstElementChild);
+// console.log(contain.previousElementSibling);
+
+
+
