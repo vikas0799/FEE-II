9c66cbc900dd2cdc45220034723b4fd26a3ef367
Vikas Patel <vikaspatel07071999@gmail.com>
2024-02-28 12:47:57 +0530

    js dom

---
+++ G10/lect-34-js dom/app.js
@@ -0,0 +1,16 @@
+// // console.log("hello g10");
+
+//    let ans=document.createElement("p");
+//    ans.innerHTML="second paragraph";
+// //    console.log(ans);
+//  let ele=document.getElementsByTagName("div");
+//  ele[0].appendChild(ans);
+// //  ele[0].appendChild(ans);
+// // console.log(ele);
+
+
+// function xyz(){
+//     // console.log("hello xyz");
+//     document.getElementsByTagName("p")[0].style.color="red";
+//     document.getElementsByTagName("p")[0].innerHTML="john wick";
+// }
