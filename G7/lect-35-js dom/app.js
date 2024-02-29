c92fc84ba1c4df53e0b131ca0c36a86bc0c99163
Vikas Patel <vikaspatel07071999@gmail.com>
2024-02-29 12:00:22 +0530

    js dom

---
+++ G7/lect-35-js dom/app.js
@@ -0,0 +1,20 @@
+console.log("hello 1");
+console.log("hello 2");
+setTimeout(() => {
+    console.log("hello 3");
+}, 4000);
+console.log("hello 4");
+
+// function foo(b) {
+//     const a = 10;
+//     return a + b + 11;
+//   }
+  
+//   function bar(x) {
+//     const y = 3;
+//     return foo(x * y);
+//   }
+  
+//   const baz = bar(7); 
+  
+  
