ea94d71751ffad6d142be4df31afc84f4d5512a1
Vikas Patel <vikaspatel07071999@gmail.com>
2024-03-20 13:37:35 +0530

    Reactjs-01

---
+++ G10/Lect-40 - Reactjs/g10/g1020/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+    <p>g10 batch</p>
+      <h1>welcome tyo g10</h1>
+      <div>
+        <p>Lorem, ipsum dolor.</p>
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ G7/lect-40 React-js/g7/src/Vikas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Vikas() {
+  return (
+    <>
+    <p>Lorem ipsum dolor sit.</p>
+    <div>Vikas</div>
+    </>
+    
+  )
+}
+
+export default Vikas
