51318696dc12278e7d5f43cec17490d23618a0c2
Vikas Patel <vikaspatel07071999@gmail.com>
2024-04-23 20:44:56 +0530

    react-router-dom

---
+++ G30/Lect-57-React-Navigation/RouterProject/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>About
+      <h1>about page</h1>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni quos eveniet itaque incidunt corporis vero repellendus velit optio, tempore tenetur asperiores hic soluta excepturi nemo? Commodi ipsam esse nobis quasi quidem numquam eligendi! Temporibus itaque nihil tempora dolorem sequi laborum a aliquam ratione explicabo hic. Libero, itaque, tenetur ratione architecto assumenda sunt atque neque modi quo veniam rem omnis? Eius dolores sit, molestiae assumenda qui illo tempore aliquam? Fugiat necessitatibus accusantium eum veniam vitae excepturi aspernatur. Harum, consectetur magni a molestiae distinctio veritatis laboriosam porro vero nulla expedita obcaecati deleniti, veniam, explicabo eveniet vitae nostrum animi tempore dolor omnis praesentium. Excepturi, sint earum. Fugiat esse velit magni, inventore laboriosam recusandae illo error perspiciatis illum ipsam accusamus aliquid cum at eos suscipit tempora eveniet eius consequuntur aliquam beatae natus commodi nihil architecto optio? Repellat similique eius voluptas laborum voluptate sapiente voluptates. Totam quo officiis ducimus, perspiciatis aspernatur accusamus! Aliquid debitis facilis nemo ea libero ab, accusantium asperiores ipsam dignissimos eligendi omnis, laudantium laboriosam quisquam similique quae maiores exercitationem fugit atque magnam aut dolores culpa ad tempora quia! Odio dolore et incidunt impedit harum iste ipsum nemo, facere minus vel iusto quas veniam maiores sed quaerat, nihil esse praesentium. Recusandae magnam obcaecati praesentium harum ipsam inventore magni, laboriosam id aperiam quam eaque, quidem assumenda doloremque exercitationem deserunt culpa perspiciatis minus ab repellat iure? Numquam aspernatur porro deserunt doloribus cum blanditiis sapiente iusto?</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ G30/Lect-57-React-Navigation/RouterProject/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>Contact
+      <h1>constact from</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro in dolorum quaerat sapiente inventore laborum? Quibusdam sint non quos a exercitationem nulla repellat iste explicabo dolor nobis, itaque distinctio inventore, dignissimos illum libero! Perferendis, earum debitis quis eos molestiae ipsam porro ad maxime, mollitia ipsum deserunt optio. Quos vitae ducimus fuga enim? In et harum necessitatibus ipsum quidem non? Ab aperiam praesentium commodi recusandae voluptas excepturi exercitationem rem sunt, dolores architecto fugiat adipisci laboriosam quaerat libero quod. Minima eveniet necessitatibus eius blanditiis ratione repellendus nostrum voluptate et minus qui placeat quidem quos, recusandae eligendi, id laboriosam dolorem unde expedita sequi eum natus consequuntur quam ipsum fugiat! Placeat dolores facilis molestiae. Sequi, commodi. Fugiat, accusantium! Commodi non sit iste asperiores ducimus ipsam reiciendis itaque, nisi ratione nobis harum deserunt dignissimos, illo, ullam placeat maiores! Cum veritatis nostrum voluptas, velit nam, possimus repudiandae minima eaque molestias omnis consequatur sunt vitae sapiente quis, iste vel illo! Eveniet facere ab, dolore eos repudiandae mollitia cumque dolores adipisci voluptatum facilis eligendi dicta quaerat enim atque nisi totam exercitationem incidunt temporibus iste rem tempora corrupti blanditiis! Sit dolorem laborum, ipsam architecto officia vero assumenda harum optio iusto nihil inventore, a aliquam, nostrum amet accusamus odio possimus hic. Provident magnam laboriosam cupiditate reprehenderit vitae, aut accusantium nisi nobis exercitationem nemo distinctio corporis ut tempora repellat explicabo! Ex quidem quis quibusdam harum pariatur consectetur amet enim necessitatibus sunt magni id aliquam ad, molestias numquam dicta nemo vel!</h1>
+    </div>
+  )
+}
+
+export default Contact
