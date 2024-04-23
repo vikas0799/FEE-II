8757306a500abdef6d9d0fda15b915de5de36d54
Vikas Patel <vikaspatel07071999@gmail.com>
2024-04-23 11:10:45 +0530

    react-router-dom

---
+++ G7/Lect-57-React-Navigation/RouterProject/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Home
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor maxime quaerat distinctio quasi temporibus rem fuga excepturi assumenda repellendus, sequi debitis tempore expedita sapiente, eum provident iste asperiores dolore voluptatem adipisci? Rem officia, odio consequuntur repudiandae earum ad corrupti! Atque praesentium ea, nisi sequi sapiente dolores eveniet voluptates voluptatibus nihil nobis dolore veniam error quas, molestiae earum ut, harum consequuntur cumque voluptatum at accusamus rem eum. Adipisci cumque tempora maiores, sit iusto nobis nihil officia explicabo deleniti id distinctio vitae laborum, labore optio esse, minima quas. Nemo quisquam accusantium vel illo ad libero esse sed ex obcaecati culpa aliquid praesentium mollitia consequatur consectetur a, consequuntur repudiandae provident cumque? Voluptatum aut nisi iste animi placeat provident cumque accusamus accusantium temporibus. Dolore, totam mollitia similique fugiat sapiente, quos fugit neque aut molestiae in placeat reiciendis accusamus numquam veritatis illum nemo nisi? Reprehenderit, cumque natus! Iste doloribus dolorem accusamus minus distinctio eum vitae laboriosam debitis quo, veritatis provident aliquam ratione tempore quibusdam sunt corrupti! Ea eveniet officia natus illum quis repudiandae, ipsum quas nisi alias earum animi veritatis, rem id minima. Id, ex! Amet sit quod dignissimos ea, perferendis ducimus iure eveniet porro, laudantium id quia repellat. Provident porro repudiandae libero amet. Totam sunt illum in, quia quidem ratione sapiente, a aliquid dolor maiores aspernatur enim officia facere ducimus molestias veniam quo amet? Porro distinctio dicta a itaque est alias neque repellendus explicabo!</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ G7/Lect-57-React-Navigation/RouterProject/src/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div>
+      <h2>Services</h2>
+      <p>Explore our range of services and offerings.</p>
+    </div>
+  );
+}
+
+export default Services;
